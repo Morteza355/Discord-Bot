8384d1a933170edad6b7a4946766690475ad42b6
Morteza355 <74528563+Morteza355@users.noreply.github.com>
2020-12-28 18:21:52 +0330

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -101,6 +101,5 @@ bot.Stats = {
 	bot.mongoose.init(bot);
 
 	// Connect bot to discord API
-	const token = process.env.token;
-	bot.login(token).catch(e => bot.logger.error(e.message));
+	bot.login(process.env.token);
 })();
